5de98a02fdf881d942b82507c91d990f3ceae34c
Ayşenur Ekiz <94220967+Monierrioir@users.noreply.github.com>
2024-05-29 10:12:15 +0300

    Update loop.asm

--- loop.asm
+++ loop.asm
@@ -25,3 +25,32 @@ ADD AL, AL
 LOOP label  ; if CX not zero jump another label
 
 ret 
+
+MOV AL, 3d
+MOV CH, 5h  ;CH,CL= CX 
+            ;05,00=0500h (overload)
+label:
+
+INC AL
+INC AL
+ADD AL, AL
+
+LOOP label  ;
+
+ret 
+
+
+MOV AL, 0000h  
+MOV CX, 100d   
+
+
+label: 
+
+ADD AL,CX  
+
+LOOP label 
+
+MOV BX, 0100h 
+MOV DS,BX 
+MOV [1000h], AX
+ret 
