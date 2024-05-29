a02903e187f000923d02dec302ac07722001fa3e
Ayşenur Ekiz <94220967+Monierrioir@users.noreply.github.com>
2024-05-29 10:57:37 +0300

    Add files via upload

---
+++ jump.asm
@@ -0,0 +1,42 @@
+MOV AL, 5
+MOV CL, 7
+
+label:
+INC AL
+
+DEC AL
+
+JMP label
+
+ret
+
+
+MOV AL, 5
+MOV CL, 7
+
+label:
+INC AL
+
+DEC AL
+
+JE label  ;if equal zero jump
+
+ret 
+
+
+MOV AL, 5
+MOV CL, 7
+
+label:
+INC AL
+
+DEC CL
+
+JNE label   ;if not equal zero jump
+
+ret
+
+
+
+
+
